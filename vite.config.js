740c2d2ed523556239a8206d2f8aed751c0aa8f8
WisdomPro <145395835+ewislla@users.noreply.github.com>
2025-08-30 14:10:13 +0000

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,43 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
   server: {
-    host: '0.0.0.0',
-    allowedHosts: ['.replit.dev', '.repl.co'],
+    host: "0.0.0.0",
+    allowedHosts: [".replit.dev", ".repl.co"],
   },
   define: {
-    global: 'globalThis',
-    'process.env': '{}',
-    'process.version': '"v16.0.0"',
-    'process.platform': '"browser"',
-    'process.browser': 'true',
+    global: "globalThis",
+    "process.env": "{}",
+    "process.version": '"v16.0.0"',
+    "process.platform": '"browser"',
+    "process.browser": "true",
   },
   resolve: {
     alias: {
-      buffer: 'buffer',
-      stream: 'stream-browserify',
-      http: 'stream-http',
-      https: 'https-browserify',
-      url: 'url',
-      crypto: 'crypto-browserify',
-      util: 'util',
+      buffer: "buffer",
+      stream: "stream-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      url: "url",
+      crypto: "crypto-browserify",
+      util: "util",
     },
   },
   optimizeDeps: {
     include: [
-      'buffer',
-      'stream-browserify',
-      'stream-http',
-      'https-browserify',
-      'url',
-      'crypto-browserify',
-      'util',
+      "buffer",
+      "stream-browserify",
+      "stream-http",
+      "https-browserify",
+      "url",
+      "crypto-browserify",
+      "util",
     ],
   },
   build: {
