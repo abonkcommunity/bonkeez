02be9bf0acbef3e07be1bd99e1a54f093097660d
WisdomPro <145395835+ewislla@users.noreply.github.com>
2025-09-03 23:40:41 +0000

    Pa

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "oogphwzqrysxignqsvmd.supabase.co"
+const supabaseUrl = "https://oogphwzqrysxignqsvmd.supabase.co"
 
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vZ3Bod3pxcnlzeGlnbnFzdm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3OTEyMzEsImV4cCI6MjA3MjM2NzIzMX0.n-DT4EP8H-63akqGbSR7JiwPknMRaDuk_mD5wR7NbOo"
 
